61635068696cd0e80225d065c2681b4e2abca986
Francesco <goras.francesco@gmail.com>
2021-03-20 17:32:41 +1300

    Add shaders (with capitalized folder! WOW!)

---
+++ Shaders/Entity-Vertex.glsl
@@ -0,0 +1,37 @@
+#version 330 core
+
+uniform mat4 projMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 modelMatrix;
+uniform vec3 lightPosition;
+
+layout (location = 0) in vec3 vertex;
+layout (location = 1) in vec3 normal;
+layout (location = 2) in vec2 textureCoords;
+
+out vec2 texCoords;
+out vec3 surfaceNormal;
+out vec3 toLightVector;
+out vec3 toCameraVector;
+out float visibility;
+
+const float density = 0.0013;
+const float gradient = 5;
+
+void main() {
+	//positions
+	vec4 worldPosition = modelMatrix * vec4(vertex, 1.0);
+	vec4 positionRelativeToCam = viewMatrix * worldPosition;
+	gl_Position = projMatrix * positionRelativeToCam;
+
+	//lighting (world space)
+	surfaceNormal = (modelMatrix * vec4(normal, 0.0)).xyz;
+	toLightVector = lightPosition - worldPosition.xyz;
+	toCameraVector = (inverse(viewMatrix) * vec4(0.0, 0.0, 0.0, 1.0)).xyz - worldPosition.xyz;
+
+	//distance fade
+	float distance = length(positionRelativeToCam.xyz);
+	visibility = exp(-pow((distance*density), gradient));
+
+	texCoords = textureCoords; //pass tex
+}
